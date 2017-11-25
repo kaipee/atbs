6aaae2abc792a3b581dc31bb323331be29d02612
Keith Patton <github@kaipee.co.uk>
2017-11-25 20:22:49 +0000

    playing with functions

---
+++ chapter_3/helloFunc.py
@@ -0,0 +1,10 @@
+# PLAYING WITH FUNCTIONS
+
+def hello():                # Define the hello function
+    print('Howdy!')         # Print 3 lines stating Howdy, Howdy and Hello There
+    print('Howdy!!!')
+    print('Hello There.')
+
+hello()                     # Call hello function 3 times
+hello()
+hello()
