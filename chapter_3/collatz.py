30bad61c2abb42c4d5f34812a7c7b63dc6299f81
Keith Patton <github@kaipee.co.uk>
2017-11-25 20:10:55 +0000

    added comments

--- chapter_3/collatz.py
+++ chapter_3/collatz.py
@@ -1,24 +1,24 @@
 # THE COLLATZ SQUEEZE
 
-def collatz(number):                # Function named collatz that has one paramter named "number"
-    if (number % 2) == 0:           # Check if number is even
-        result = (number // 2)      # If even, get the result of (number // 2)
-        print(result)               # Print the result
-        return result               # Return the result
-    elif (number % 2) == 1:         # Check if number is odd
-        result = (3 * number + 1)   # If odd, get the result of (3 * number + 1)
-        print(result)               # Print the result
-        return result               # Return the result
+def collatz(number):                      # Function named collatz that has one paramter named "number"
+    if (number % 2) == 0:                 # Check if number is even
+        result = (number // 2)            # If even, get the result of (number // 2)
+        print(result)                     # Print the result
+        return result                     # Return the result
+    elif (number % 2) == 1:               # Check if number is odd
+        result = (3 * number + 1)         # If odd, get the result of (3 * number + 1)
+        print(result)                     # Print the result
+        return result                     # Return the result
 
-def getValue():
-    global i
-    i = input('Please input a number : ')   # Get input from user
+def getValue():                           # Define function for obtaining user input
+    global i                              # Call global function i
+    i = input('Please input a number : ') # Get input from user and assign to global variable
 
-getValue()
-while i != 1:                       # Execute function while input value is not 1
-    try:
-        i = collatz(int(i))         # Assign i to return value of collatz and execute collatz function on i
-    except ValueError:
+getValue()                                # Call function to obtain user unput
+while i != 1:                             # Execute function while input value is not 1
+    try:                                  # Handle integers
+        i = collatz(int(i))               # Assign i to return value of collatz and execute collatz function on i
+    except ValueError:                    # Handle non-integers
+        print('Error: Not a NUMBER!')     # Notify user of non-integer
         print()
-        print('Error: Not a NUMBER!')
-        getValue()
+        getValue()                        # Re-call function to create sense of continuing program
