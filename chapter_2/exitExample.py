073301ef427bd011019c1a9da9adc879f082a42f
Keith Patton <github@kaipee.co.uk>
2017-11-25 19:44:47 +0000

    exiting a program with sys.exit()

---
+++ chapter_2/exitExample.py
@@ -0,0 +1,10 @@
+# Exiting early with sys.exit()
+
+import sys
+
+while True:
+    print('Type exit to exit.')
+    response = input()
+    if response == 'exit':
+        sys.exit()
+    print('You incorrectly typed ' + response + '.')
